372354f4dd22cff2948408dabc2ec56eb95bf81c
Skyl9 <tristan.rigaud@nrmi.fr>
2026-06-22 18:28:29 +0200

    feat(ui): améliore l'accessibilité de GraphCanvas avec une liste interactive de nœuds
    - Ajoute une boîte cachée visuellement avec `aria-live` pour une meilleure accessibilité
    - Intègre une liste interactive permettant la sélection des nœuds depuis l'interface avec gestion via `useGraphStore`

--- src/components/GraphCanvas.tsx
+++ src/components/GraphCanvas.tsx
@@ -1,18 +1,37 @@
 import { Canvas } from "@react-three/fiber";
 import Scene from "../scene/Scene";
 import { Graph } from "../types/ApiTypes/graph";
+import { useGraphStore } from "../stores/useGraphStore";
+import { visuallyHidden } from "@mui/utils";
+import { Box } from "@mui/material";
 
 interface GraphCanvasProps {
   graphData: Graph;
 }
 
 export default function GraphCanvas({ graphData }: GraphCanvasProps) {
+  const setSelectedNodeId = useGraphStore((s) => s.setSelectedNodeId);
+
   return (
-    <Canvas
-      gl={{ antialias: true, alpha: true }}
-      style={{ background: "transparent" }}
-    >
-      <Scene graphData={graphData} />
-    </Canvas>
+    <>
+      <Box sx={visuallyHidden} aria-live="polite">
+        <h2>Liste des nœuds du graphe</h2>
+        <ul>
+          {graphData.nodes.map((node) => (
+            <li key={node.id}>
+              <button onClick={() => setSelectedNodeId(node.id)}>
+                Sélectionner le nœud {node.nom}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </Box>
+      <Canvas
+        gl={{ antialias: true, alpha: true }}
+        style={{ background: "transparent" }}
+      >
+        <Scene graphData={graphData} />
+      </Canvas>
+    </>
   );
 }
