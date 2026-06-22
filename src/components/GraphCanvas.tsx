39f7d61eee637d83d7c0968da70d05d9247c573f
Skyl9 <tristan.rigaud@nrmi.fr>
2026-06-22 19:00:26 +0200

    feat(ui): ajoute les statistiques de rendu et un log de debug dans GraphCanvas
    - Intègre le composant `Stats` pour afficher des métriques de performance en temps réel
    - Ajoute un log de debug avec le nombre de nœuds et d'arêtes lors du montage du composant

--- src/components/GraphCanvas.tsx
+++ src/components/GraphCanvas.tsx
@@ -1,17 +1,26 @@
 import { Canvas } from "@react-three/fiber";
+import { Stats } from "@react-three/drei";
 import Scene from "../scene/Scene";
 import { Graph } from "../types/ApiTypes/graph";
+import { useEffect } from "react";
 
 interface GraphCanvasProps {
   graphData: Graph;
 }
 
 export default function GraphCanvas({ graphData }: GraphCanvasProps) {
+  useEffect(() => {
+    console.log(
+      `[DEBUG R3F] Graphe monté avec ${graphData.nodes.length} nœuds et ${graphData.edges.length} arêtes.`,
+    );
+  }, [graphData]);
+
   return (
     <Canvas
       gl={{ antialias: true, alpha: true }}
       style={{ background: "transparent" }}
     >
+      <Stats />
       <Scene graphData={graphData} />
     </Canvas>
   );
